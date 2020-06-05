e301e98d15b59707d9f841aba93071f70e5394bf
Bob Fang <bob.fang.london@gmail.com>
2020-06-05 09:38:46 +0000

    Use custom types for py types

--- tests/python-tests/test_api.py
+++ tests/python-tests/test_api.py
@@ -1,3 +1,11 @@
 import pytest
 
 import pytomlpp
+
+
+def test_keys():
+    toml_string = "a = 3"
+    table = pytomlpp.loads(toml_string)
+    keys = table.keys()
+    assert len(keys) == 1
+    assert keys[0] == 'a'
