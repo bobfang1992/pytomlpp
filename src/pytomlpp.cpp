26e57ebb54a30f7e7f1732942a5083a6498edc66
Bob Fang <bob.fang.london@gmail.com>
2020-06-07 09:38:05 +0000

    add toml-test for testing

--- src/pytomlpp.cpp
+++ src/pytomlpp.cpp
@@ -1,3 +1,5 @@
+#include <fstream> //required for toml::parse_file()
+
 #include <pytomlpp.hpp>
 
 py::dict loads(std::string_view toml_stirng) {
@@ -5,8 +7,14 @@ py::dict loads(std::string_view toml_stirng) {
     return pytomlpp::table_to_dict(tbl);
 }
 
+py::dict load(std::string file_name) {
+    auto tbl = toml::parse_file(file_name);
+    return pytomlpp::table_to_dict(tbl);
+}
+
 PYBIND11_MODULE(pytomlpp, m) {
     m.doc() = "tomlplusplus python wrapper";
     m.def("loads", &loads);
+    m.def("load", &load);
 }
 
--- tests/python-tests/test_api.py
+++ tests/python-tests/test_api.py
@@ -1,7 +1,18 @@
 import pytest
+import glob
+import os
+import pathlib
 
 import pytomlpp
 
+EXCLUDE_LIST = ["empty"]
+
+@pytest.fixture
+def valid_toml_files():
+    current_path = os.path.dirname(__file__)
+    toml_files = glob.glob(current_path + "/../toml-test/tests/valid/*.toml")
+    return [pathlib.Path(p) for p in toml_files]
+         
 
 def test_keys():
     toml_string = "a = 3"
@@ -9,3 +20,11 @@ def test_keys():
     keys = table.keys()
     assert len(keys) == 1
     assert list(keys)[0] == "a"
+
+def test_valid_toml_files(valid_toml_files):
+    for t in valid_toml_files:
+        if t.stem in EXCLUDE_LIST:
+            continue
+        print(f"parsing {t}")
+        table = pytomlpp.load(str(t))
+        assert type(table) == dict
