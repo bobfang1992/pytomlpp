f9d7c416ae5310115bc226b853147d24d6895318
Clément Robert <cr52@protonmail.com>
2021-05-23 14:04:21 +0200

    feature: add support for io operations from filenames (#52)
    * feature: add support for io operations from filenames
    
    * refactor: do not expose encoding parameter (TOML is utf-8 only)
    
    * convert pathlib.Path objects to str in test to support Python 3.5

--- src/pytomlpp/_io.py
+++ src/pytomlpp/_io.py
@@ -3,25 +3,47 @@
 from . import _impl
 
 
-def dump(data, fl):
+def dump(data, fl, mode="w"):
     """Serialise data to TOML.
 
     Args:
         data: data to serialise
-        fl (io.TextIOBase): file-object to write to (supports ``write``)
+        fl (io.TextIOBase, str or os.Pathlike):
+            file-object to write to (supports ``write``)
+            or a file name (str, os.Pathlike) that supports ``open``
+        mode (str): opening mode. Either "w" or "wt" (text), or "wb" (binary). Defaults to "w".
+            Ignored if fl supports ``write``.
     """
+    data = _impl.dumps(data)
+    if mode == "wb":
+        data = data.encode("utf-8")
+    if hasattr(fl, "write"):
+        fl.write(data)
+        return
+    with open(fl, mode=mode) as fh:
+        fh.write(data)
 
-    fl.write(_impl.dumps(data))
 
 
-def load(fl):
+def load(fl, mode="r"):
     """Deserialise from TOML.
 
     Args:
-        fl (io.TextIOBase): file-object to read from (supports ``read``)
+        fl (io.TextIOBase, str or os.Pathlike):
+            file-object to read from (supports ``read``)
+            or a file name (str, os.Pathlike) that supports ``open``
+        mode (str):  opening mode. Either "r" or "rt" (text) or "rb" (binary). Defaults to "r".
+            Ignored if fl supports ``read``.
 
     Returns:
         deserialised data
     """
 
-    return _impl.loads(fl.read())
+    if hasattr(fl, "read"):
+        data = fl.read()
+    else:
+        with open(fl, mode=mode) as fh:
+            data = fh.read()
+    if isinstance(data, bytes):
+        return _impl.loads(data.decode("utf-8"))
+    return _impl.loads(data)
