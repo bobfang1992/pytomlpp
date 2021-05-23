80b98a21fdf7b372f7bb303eed0a6001799a42d6
Mark Gillard <marzer_@hotmail.com>
2021-05-23 16:00:17 +0300

    update toml++ to 2.4.0
    drive-bys:
    - minor improvements to benchmark script and README

--- benchmark/run.py
+++ benchmark/run.py
@@ -4,16 +4,34 @@ import tomlkit
 import qtoml
 import timeit
 
-def benchmark(name, func, number=5000):
+def benchmark(name, run_count, func, compare_to=None):
     print(f'{name:>10}: Running...', end='', flush=True)
-    res = str(timeit.timeit(func, number=number)).split('.')
-    print('\b'*10 + f'{res[0]:>3}.{res[1]} s')
+    time_taken = timeit.timeit(func, number=run_count)
+    res = str(time_taken).split('.')
+    print('\b'*10, end='')
+    print(f'{res[0]:>4}.{res[1][:3]} s', end='')
+    if compare_to is not None:
+        num = time_taken
+        denom = compare_to
+        relation = 'slower'
+        if num < denom:
+            num = compare_to
+            denom = compare_to
+            relation = 'faster'
+        delta = int((num / denom) * 10.0) / 10.0
+        print(f' ({delta}x {relation})', end='')
+    print('')
+    return time_taken
 
-test_data = ''
-with open('data.toml', 'r', encoding='utf-8') as f:
-    test_data = f.read()
+def run(run_count = 5000):
+    test_data = ''
+    with open('data.toml', 'r', encoding='utf-8') as f:
+        test_data = f.read()
+    print(f'Parsing data.toml {run_count} times:')
+    baseline = benchmark('pytomlpp', run_count, lambda: pytomlpp.loads(test_data))
+    benchmark('toml', run_count, lambda: toml.loads(test_data), compare_to=baseline)
+    benchmark('qtoml', run_count, lambda: qtoml.loads(test_data), compare_to=baseline)
+    benchmark('tomlkit', run_count, lambda: tomlkit.parse(test_data), compare_to=baseline)
 
-benchmark('pytomlpp', lambda: pytomlpp.loads(test_data))
-benchmark('toml', lambda: toml.loads(test_data))
-benchmark('qtoml', lambda: qtoml.loads(test_data))
-benchmark('tomlkit', lambda: tomlkit.parse(test_data))
+if __name__ == '__main__':
+    run()
