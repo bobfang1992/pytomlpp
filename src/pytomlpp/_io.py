948fba74bf15554b32711db9438f862d677fe34e
Backist <alvarodrumer54@gmail.com>
2024-01-02 16:14:19 +0100

    Hotfix, syntax error.

--- src/pytomlpp/_io.py
+++ src/pytomlpp/_io.py
@@ -33,7 +33,7 @@ def dump(data: Dict[Any, Any], fl: FilePathOrObject, mode: str = "w", encoding: 
     data = _impl.dumps(data)
     if mode == "wb":
         encoding = None
-        data = data.encode("utf-8")
+        data     = data.encode("utf-8")
     if hasattr(fl, "write"):
         fl.write(data)
         return
